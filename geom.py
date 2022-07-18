03059ffe929b2077e280ed6106a3809554188dae
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-18 18:32:37 -0400

    work in linalg stuffs

--- geom.py
+++ geom.py
@@ -1,39 +1,21 @@
 # Kaidun (by HktOverload)
 
-from collections import namedtuple
+from linalg import *
 
 # A geometry
 Geom = namedtuple('Geom', ('verts', 'tris', 'aabb'))
 
-# A camera
-Cam = namedtuple('Cam', ('ctr', 'size', 'flen'))
+def makeAABB(verts):
+    -...
 
-# Convert to the camera's coordinate space
-def camcoorder(cam, x):
-    a, b, c = cam.ctr
-    x, y, z = x
-    return (x-a, y-b, c-z)
+class Geom(object):
+    __slots__ = 'tris', 'verts', 'aabb', 'offset'
+    def __init__(self, tris, offset):
+        self.tris = tris
+        self.verts = list({ j for i in self.tris for j in i })
+        self.aabb = makeAABB(self.verts)
+        self.offset = offset
 
-# 3x3 Matrix-vector multiplication
-# The matrix should be in column-major order
-def mat3x3(mat, x): 
-    return (
-        (x[0]*mat[0]) + (x[1]*mat[3]) + (x[2]*mat[6]),
-        (x[0]*mat[1]) + (x[1]*mat[4]) + (x[2]*mat[7]),
-        (x[0]*mat[2]) + (x[1]*mat[5]) + (x[2]*mat[8]),
-    )
-
-# Scaling an n-dimensional vector
-def scale(x, scalar):
-    return ( *( i*scalar for i in x ), )
-
-# Perform a complete perspective transform
-def persp(cam, x):
-    x, y, z = camcoorder(cam, x)
-    return scale(
-        (
-            x / (z*cam.size[0]),
-            y / (z*cam.size[1]),
-        ),
-        cam.flen,
-    )
+# Perform a perspective transform
+def persp(cam, v):
+    cameraMat = 
