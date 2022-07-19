a2f4c3fb04cb936481581c6ec0dfa23e4106f64d
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-19 15:26:48 -0400

    IT WORKS!!!!!! (no rotation but idc tbh)

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from worlds import *
 from viewport import *
 
 def appStarted(app):
+    app.camCtr = [0, 0, -1]
     app.world = CubeWld()
-    app.viewer = Viewport(Camera([0, 0, -2]), app.world)
+    app.viewer = Viewport(Camera(app.camCtr), app.world)
     # The size changed from undefined to something, didn't it?
     sizeChanged(app)
 
@@ -17,13 +18,23 @@ def sizeChanged(app):
     app.cx, app.cy = app.w//2, app.h//2
 
 def keyPressed(app, event):
+    step = 0.2
     if event.key == 'a':
-        app.viewer.cam.ctr[0] -= 0.2
-    elif event.key == 'b':
-        app.viewer.cam.ctr[0] += 0.2
+        app.camCtr[0] -= step
+    elif event.key == 'd':
+        app.camCtr[0] += step
+    elif event.key == 'w':
+        app.camCtr[1] -= step
+    elif event.key == 's':
+        app.camCtr[1] += step
+    elif event.key == 'e':
+        app.camCtr[2] -= step
+    elif event.key == 'c':
+        app.camCtr[2] += step
 
 def redrawAll(app, canvas):
     canvas.create_rectangle(0, 0, app.w, app.h, fill='#000')
+    app.viewer.cam = Camera(app.camCtr)
     app.viewer.render(canvas)
 
 if __name__ == '__main__':
