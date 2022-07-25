95c5cf451d9e7af7e5a509de768ccb3061a62c6e
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-25 13:51:42 -0400

    wheeeee the thiiirdd dimmessnioionnnn

--- scene_components.py
+++ scene_components.py
@@ -9,10 +9,18 @@ def moveCamera(app, event):
         app.viewer.cam.rotate(pitch=+0.1)
     elif event == ('kb/p', 'Right'):
         app.viewer.cam.rotate(pitch=-0.1)
+    elif event == ('kb/p', 'w'):
+        app.viewer.cam.ctr[1] -= 0.1
+    elif event == ('kb/p', 's'):
+        app.viewer.cam.ctr[1] += 0.1
     elif event == ('kb/p', 'a'):
         app.viewer.cam.ctr[0] -= 0.1
     elif event == ('kb/p', 'd'):
         app.viewer.cam.ctr[0] += 0.1
+    elif event == ('kb/p', 'e'):
+        app.viewer.cam.ctr[2] -= 0.1
+    elif event == ('kb/p', 'c'):
+        app.viewer.cam.ctr[2] += 0.1
 
 def adjustLight(app, event):
     if event == ('kb/p', '<'):
