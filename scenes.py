9d0e3d9fa26917330d082ff42ab7c9d3673e01d0
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-27 19:09:01 -0400

    `ChunkScene` was actually dumb :(

--- scenes.py
+++ scenes.py
@@ -2,12 +2,41 @@
 
 import abc
 
-from scene_types import *
+from chunks import *
+
 from scene_geometry import *
 from scene_components import *
 
 import rendering as r
 import lighting as l
+
+class Scene(abc.ABC):
+
+    @classmethod
+    @abc.abstractmethod
+    def onEvent(cls, app, event):
+        # Called when an event occurs
+        # These don't use the same format as tkinter
+        # For example, pressing 'r' is encoded as ('kb/p', 'r')
+        # Moving the mouse to (0, 0) is encoded as ('mouse/to', 0, 0)
+        pass
+
+    @classmethod
+    def allGeometry(cls, app):
+        # A list containing (Geom, Renderer) tuples for everything
+        # within this scene
+        # Basically just a geomsrc
+        # This is not an abstract method because implementing it
+        # is optional, not every scene has 3D geometry
+        return []
+
+    @classmethod
+    def drawOverlay(cls, app, canvas):
+        # A procedure to draw any non-3D elements that go on top
+        # of the 3D part of the scene
+        # This is not an abstractmethod because implementing it
+        # is completely optional
+        pass
 
 class CubeScene(Scene):
 
