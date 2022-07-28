250218c1e69f7f6658d88e4f92658937b1450ae0
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-28 11:28:40 -0400

    `Chunk` methods

--- chunks.py
+++ chunks.py
@@ -2,16 +2,39 @@
 
 from geom import *
 
+import rendering as r
+
+def getStdRenderer(dist):
+	if dist < 3:
+		return r.flat
+	else:
+		return r.wireframe
+
 class Chunks(object):
 	__slots__ = 'indexed', 'xySize'
-	def __init__(self, indexed, xySize):
+	def __init__(self, indexed, xySize, rendererFn=getStdRenderer):
 		self.indexed = indexed
 		self.xySize = xySize
+		self.rendererFn = rendererFn
 
 	def loaded(self, x, y, chunkDist):
 		xIndex, yIndex = x // self.xySize, y // self.xySize
 		return [
-			self.indexed[(xIndex+i, yIndex+j)] \
+			self.getChunk(
+				xIndex+i, yIndex+j,
+				self.hypotXY(i, j)
+			) \
 				for i in range(-chunkDist, +chunkDist+1)
 				for j in range(-chunkDist, +chunkDist+1)
+				if self.hasChunk(xIndex+i, yIndex+j)
 		]
+
+	def hypotXY(self, cX, cY):
+		return math.hypot(cX * self.xySize, cY * self.xySize)
+
+	def getChunk(self, x, y, dist):
+		geom = self.indexed[(x, y)]
+		return (self.rendererFn)(dist)(geom)
+
+	def hasChunk(self, x, y):
+		return (x, y) in self.indexed
