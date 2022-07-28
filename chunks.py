2025dc5544945518c8be0862ff473148491166ee
HktOverload <102832670+HktOverload@users.noreply.github.com>
2022-07-28 11:13:43 -0400

    chunks

--- chunks.py
+++ chunks.py
@@ -3,7 +3,15 @@
 from geom import *
 
 class Chunks(object):
-	__slots__ = 'indexed', 'size'
-	def __init__(self, size):
-		self.indexed = {}
-		self.size = size
+	__slots__ = 'indexed', 'xySize'
+	def __init__(self, indexed, xySize):
+		self.indexed = indexed
+		self.xySize = xySize
+
+	def loaded(self, x, y, chunkDist):
+		xIndex, yIndex = x // self.xySize, y // self.xySize
+		return [
+			self.indexed[(xIndex+i, yIndex+j)] \
+				for i in range(-chunkDist, +chunkDist+1)
+				for j in range(-chunkDist, +chunkDist+1)
+		]
